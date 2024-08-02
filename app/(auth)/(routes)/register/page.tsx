fde141f2d766d23263d8d7efa948f98110deb5d9
idityaGE <am44910606@gmail.com>
2024-08-03 00:36:58 +0530

    docker clone app && project setup and folder structure is completed

---
+++ app/(auth)/(routes)/register/page.tsx
@@ -0,0 +1,10 @@
+const RegisterPage = () => {
+  return (
+    <>
+      <h1>Register</h1>
+      <p>This is the register page</p>
+    </>
+  );
+}
+
+export default RegisterPage;
---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,12 @@
+
+
+
+export default function Home() {
+  return (
+    <>
+    <div className="">
+      This is a protected route
+    </div>
+    </>
+  );
+}
