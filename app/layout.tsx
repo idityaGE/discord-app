44ed489d1e273452a524116f5edc8b24c25ec0db
idityaGE <am44910606@gmail.com>
2024-08-06 00:09:33 +0530

    uploadThing Setup is done

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,9 @@ import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/components/providers/theme-provider"
 import { cn } from "@/lib/utils";
+import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
+import { extractRouterConfig } from "uploadthing/server";
+import { ourFileRouter } from "@/app/api/uploadthing/core";
 
 const font = Open_Sans({ subsets: ["latin"] });
 
@@ -24,6 +27,9 @@ export default function RootLayout({
           font.className,
           "bg-white dark:bg-[#313338]"
         )}>
+          <NextSSRPlugin
+            routerConfig={extractRouterConfig(ourFileRouter)}
+          />
           <ThemeProvider
             attribute="class"
             defaultTheme="dark"
