bdc178c7063010ebf9f0a91bcd461d2ce178846a
idityaGE <am44910606@gmail.com>
2024-09-10 17:02:35 +0530

    delete to start

---
+++ app/api/members/[memberId]/route.ts
@@ -0,0 +1,58 @@
+import { currentProfile } from "@/lib/currect-profile";
+import prisma from "@/lib/db";
+import { NextResponse } from "next/server";
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { memberId: string } }
+) {
+  try {
+    const profile = await currentProfile();
+    const { searchParams } = new URL(req.url);
+    const { role } = await req.json();
+    const serverId = searchParams.get("serverId");
+    if (!profile) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+    if (!serverId) {
+      return new NextResponse("Server Id Missing", { status: 400 });
+    }
+    if (!params.memberId) {
+      return new NextResponse("Member Id Missing", { status: 400 });
+    }
+    if (!role) {
+      return new NextResponse("Role Missing", { status: 400 });
+    }
+    // Update the role of the member in the server
+    const server = await prisma.server.update({
+      where: { id: serverId, profileId: profile.id },
+      data: {
+        members: {
+          update: {
+            where: {
+              id: params.memberId,
+              profileId: {
+                not: profile.id,
+              }
+            },
+            data: { role },
+          },
+        },
+      },
+      include: {
+        members: {
+          include: {
+            profile: true,
+          },
+          orderBy: {
+            role: "asc",
+          }
+        },
+      }
+    });
+    return NextResponse.json(server);
+  } catch (error) {
+    console.error("[MEMBERS_ID_PATCH]", error);
+    return new NextResponse("Internal server error", { status: 500 });
+  }
+}
--- components/providers/model-provider.tsx
+++ components/providers/model-provider.tsx
@@ -4,6 +4,7 @@ import { CreateServerModel } from "@/components/models/create-server-modal"
 import { useEffect, useState } from "react"
 import { InviteModel } from "../models/invite-model"
 import { EditServerModel } from "../models/edit-server-model"
+import { MembersModel } from "../models/members-model"
 
 export const ModelProvider = () => {
   const [isMounted, setIsMounted] = useState(false)
@@ -21,6 +22,7 @@ export const ModelProvider = () => {
       <CreateServerModel />
       <InviteModel />
       <EditServerModel />
+      <MembersModel />
     </>
   )
 }
