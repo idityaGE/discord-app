a855b6efe6728b58973c4d64730b87b311341c4f
idityaGE <am44910606@gmail.com>
2024-09-27 18:16:16 +0530

    final

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -8,10 +8,13 @@ import { NavigationItem } from "@/components/navigation/navigation-item";
 import { ModeToggle } from "@/components/mode-toggle";
 import { UserButton } from "@clerk/nextjs";
 import { dark } from '@clerk/themes';
+import Link from "next/link";
+import Image from "next/image";
+import { links } from "@/config";
 
 const NavigationSidebar = async () => {
   const profile = await currentProfile()
-  
+
   if (!profile) return redirect('/');
 
   const servers = await prisma.server.findMany({
@@ -25,9 +28,19 @@ const NavigationSidebar = async () => {
   })
 
   return (
-    <div
-      className="space-y-4 flex flex-col items-center h-full text-primary w-full bg-[#E3E5E8] dark:bg-[#1E1F22] py-3"
-    >
+    <div className="space-y-4 flex flex-col items-center h-full text-primary w-full bg-[#E3E5E8] dark:bg-[#1E1F22] py-3">
+      <Link href="/">
+        <Image
+          src="/logo.png"
+          alt="Discord Clone"
+          width={48}
+          height={48}
+          className="rounded-full"
+        />
+      </Link>
+
+      <Separator className="h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto" />
+      
       <NavigationAction />
       <Separator
         className="h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto"
@@ -44,6 +57,15 @@ const NavigationSidebar = async () => {
         ))}
       </ScrollArea>
       <div className="pb-3 mt-auto flex items-center flex-col gap-y-4">
+        <Link href={links.sourceCode} target="_blank" rel="noreferrer noopener">
+          <Image
+            src="/github.png"
+            alt="Source Code"
+            height={20}
+            width={20}
+            className="dark:invert-0 invert"
+          />
+        </Link>
         <ModeToggle />
         <UserButton
           afterSignOutUrl="/"
